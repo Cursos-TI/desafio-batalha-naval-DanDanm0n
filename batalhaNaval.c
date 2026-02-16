9575b42a094345803c4e8fbef8d9a7db800a4cf9
DanDanm0n <d.bello201492@gmail.com>
2026-02-16 20:46:18 -0300

    BatalhaNavalNivelAventureiro.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -10,48 +10,94 @@ int main() {
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
+    // Tamanho do tabuleiro principal
+    #define TABULEIRO 10
+
+    // Legenda visual
+    #define AGUA 0
+    #define NAVIO 3
+    #define HABILIDADE 5
+
     int tamanhotabuleiro = 10, tamanhonavio = 3;
 
     char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
     int coluna[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    int tabuleiro[10][10] = {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-    };
+    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
+    int tabuleiro[TABULEIRO][TABULEIRO] = {0};
 
-    int navio1[3] = {3, 3, 3};
+    int naviohorizontal[3] = {3, 3, 3};
 
-    int navio2[3] = {3, 3, 3};
+    int naviovertical[3] = {3, 3, 3};
 
-    tabuleiro[2][2] = navio1[0];
-    tabuleiro[2][3] = navio1[1];
-    tabuleiro[2][4] = navio1[2];
+    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
+    int naviodiagonal1[3] = {3, 3, 3};
 
-    tabuleiro[4][2] = navio2[0];
-    tabuleiro[5][2] = navio2[1];
-    tabuleiro[6][2] = navio2[2];
+    int naviodiagonal2[3] = {3, 3, 3};
 
-    if (tamanhotabuleiro>tamanhonavio) {
-        printf("O navio cabe dentro do tabuleiro\n");
+    int linhanavioH = 0;
+    int colunanavioH = 0;
+
+    int linhanavioV = 0;
+    int colunanavioV = 5;
+
+    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
+    int linhanavioD1 = 0;
+    int colunanavioD1 = 7;
+
+    int linhanavioD2 = 7;
+    int colunanavioD2 = 0;
+    
+    if (tabuleiro[linhanavioH][colunanavioH] == 0 || 
+        tabuleiro[linhanavioH][colunanavioH + 1] == 0 || 
+        tabuleiro[linhanavioH][colunanavioH + 2] == 0) {
+            tabuleiro[linhanavioH][colunanavioH] = naviohorizontal[0]; 
+            tabuleiro[linhanavioH][colunanavioH + 1] = naviohorizontal[1]; 
+            tabuleiro[linhanavioH][colunanavioH + 2] = naviohorizontal[2];
     } else {
-        printf("O navio é maior que os limites do tabuleiro\n");
+        printf("Sobreposição entre navios detectada!\n");
         return 0;
     }
 
-    if (navio1[3] != navio2[3]) {
-        printf("Os navios não se sobrepõem no mapa\n");
+    if (tabuleiro[linhanavioV][colunanavioV] == 0 || 
+        tabuleiro[linhanavioV + 1][colunanavioV] == 0 || 
+        tabuleiro[linhanavioV + 2][colunanavioV] == 0) {
+            tabuleiro[linhanavioV][colunanavioV] = naviovertical[0];
+            tabuleiro[linhanavioV + 1][colunanavioV] = naviovertical[1];
+            tabuleiro[linhanavioV + 2][colunanavioV] = naviovertical[2];
     } else {
-        printf("Os navios estão se sobrepondo no mapa");
+        printf("Sobreposição entre navios detectada!\n");
+        return 0;
+    }
+
+    if (tabuleiro[linhanavioD1][colunanavioD1] == 0 || 
+        tabuleiro[linhanavioD1 + 1][colunanavioD1 + 1] == 0 || 
+        tabuleiro[linhanavioD1 + 2][colunanavioD1 + 2] == 0) {
+            tabuleiro[linhanavioD1][colunanavioD1] = naviodiagonal1[0];
+            tabuleiro[linhanavioD1 + 1][colunanavioD1 + 1] = naviodiagonal1[1];
+            tabuleiro[linhanavioD1 + 2][colunanavioD1 + 2] = naviodiagonal1[2];
+    } else {
+        printf("Sobreposição entre navios detectada!\n");
+        return 0;
+    }
+
+    if (tabuleiro[linhanavioD2][colunanavioD2] == 0 || 
+        tabuleiro[linhanavioD2 + 1][colunanavioD2 + 1] == 0 || 
+        tabuleiro[linhanavioD2 + 2][colunanavioD2 + 2] == 0) {
+            tabuleiro[linhanavioD2][colunanavioD2] = naviodiagonal2[0];
+            tabuleiro[linhanavioD2 + 1][colunanavioD2 + 1] = naviodiagonal2[1];
+            tabuleiro[linhanavioD2 + 2][colunanavioD2 + 2] = naviodiagonal2[2];
+    } else {
+        printf("Sobreposição entre navios detectada!\n");
+        return 0;
+    }
+
+    if (tamanhotabuleiro>tamanhonavio) {
+        printf("Os navios estão dentro do limite tabuleiro\n");
+    } else {
+        printf("Os navios são maiores que os limites do tabuleiro\n");
+        return 0;
     }
     
 
@@ -73,12 +119,6 @@ int main() {
 
     return 0;
 }
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
     // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
